b5b69c67d4b4ea8726bc152308805199aa271871
parthchheda2005 <path.h.chheda@gmail.com>
2024-07-25 18:36:30 +0800

    made a small change regarding the content of the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,19 +10,27 @@ function Projects({ textGradientColor }) {
       img: fplHelper,
       repo: "https://github.com/parthchheda2005/fpl-helper",
       live: "https://fpl-helper.onrender.com",
+      title: "FPL Helper",
+      stack: "React, MongoDB, Node.js, Express.js, TailwindCSS",
     },
     {
       img: movieWebapp,
       repo: "https://github.com/parthchheda2005/movie-webapp-frontend",
       live: "https://github.com/parthchheda2005/movie-webapp-frontend",
+      title: "Movie Webapp",
+      stack: "React, MongoDB, Node.js, Express.js, CSS",
     },
     {
       img: heartDiseasePredictor,
       repo: "https://github.com/parthchheda2005/dsci-100-project/blob/main/heart_disease_report.ipynb",
+      title: "Heart Disease Predictor",
+      stack: "R, tidymodels/tidyverse",
     },
     {
       img: courseTracker,
       repo: "https://github.com/parthchheda2005/Course-Tracker",
+      stack: "Java, Java Swing, JUnit",
+      title: "Course Tracker",
     },
   ];
 
@@ -36,29 +44,33 @@ function Projects({ textGradientColor }) {
         </h1>
       </div>
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 mx-7 items-center">
+      <div className="grid sm:grid-cols-2 gap-4 mx-7 items-center">
         {images.map((el) => (
-          <div className="transform transition-all duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-neutral-900 group container rounded-md flex justify-center items-center max-h-[250px] bg-cover relative">
+          <div className="transform transition-all duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-neutral-900 group container rounded-md flex justify-center items-center max-h-[400px] bg-cover relative">
             <img src={el.img} alt="movie webapp" />
-            <div className="opacity-0 group-hover:opacity-90 bg-neutral-600 absolute inset-0 flex justify-center items-center">
-              <div className="pt-8 text-center">
-                <button
-                  onClick={() => window.open(el.repo, "_blank").focus()}
-                  className="text-center m-2 font-bold text-lg hover:scale-125 transition duration-300"
-                >
-                  <FaGithubSquare size={80} />
-                </button>
-              </div>
-              {el?.live && (
+            <div className="opacity-0 group-hover:opacity-90 bg-neutral-600 absolute inset-0 flex flex-col justify-center items-center">
+              <h1 className="text-2xl font-extrabold">{el.title}</h1>
+              <h1 className="text-xl font-extralight">{el.stack}</h1>
+              <div className="flex flex-row justify-center items-center">
                 <div className="pt-8 text-center">
                   <button
-                    onClick={() => window.open(el.live, "_blank").focus()}
-                    className="text-center m-2 py-3 px-4 rounded-lg font-bold text-black bg-neutral-50 text-lg hover:scale-125 transition duration-300"
+                    onClick={() => window.open(el.repo, "_blank").focus()}
+                    className="text-center m-2 font-bold text-lg hover:scale-125 transition duration-300"
                   >
-                    Live
+                    <FaGithubSquare size={80} />
                   </button>
                 </div>
-              )}
+                {el?.live && (
+                  <div className="pt-8 text-center">
+                    <button
+                      onClick={() => window.open(el.live, "_blank").focus()}
+                      className="text-center m-2 py-3 px-4 rounded-lg font-bold text-black bg-neutral-50 text-lg hover:scale-125 transition duration-300"
+                    >
+                      Live
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         ))}

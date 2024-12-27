c7a76324505a30084bd97e05627f1f0d391023eb
Parth Chheda <parth.h.chheda@gmail.com>
2024-12-26 16:55:11 -0800

    added a picture of my face

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,14 +1,22 @@
 import { TypeAnimation } from "react-type-animation";
 import pdf from "../assets/Parth_Chheda_Resume.pdf";
+import MyFace from "../assets/MyFace.jpg";
 
 function FrontPage({ textGradientColor }) {
   return (
     <div
       id="frontPage"
-      className="flex justify-center items-center sm:grid-cols-3 mx-auto bg-black text-white h-[70vh] py-3"
+      className="flex flex-col sm:flex-row justify-center items-center gap-8 mx-auto bg-black text-white h-[70vh] py-3 px-4 my-40 sm:my-2"
     >
-      <div className="px-5 my-auto text-center zoom-in">
-        <h1 className="text-white text-6xl lg:text-8xl font-extrabold">
+      <div className="flex justify-center">
+        <img
+          className="rounded-full w-auto h-72 sm:max-w-xs md:max-w-smlg:max-w-md object-cover"
+          src={MyFace}
+          alt="Parth Chheda"
+        />
+      </div>
+      <div className="px-5 my-auto text-center sm:text-left zoom-in">
+        <h1 className="text-white text-5xl md:text-6xl lg:text-8xl font-extrabold">
           <span className={textGradientColor}>I'm a </span>
           <br />
           <TypeAnimation
@@ -25,15 +33,18 @@ function FrontPage({ textGradientColor }) {
             repeat={Infinity}
           />
         </h1>
-        <p className="text-white sm:text-lg lg:text-xl my-6">
+        <p className="text-white text-md sm:text-lg lg:text-xl my-6">
           My name is Parth Chheda and I am a student at the University of
           British Columbia.
         </p>
-        <div className="my-3">
+
+        <div className="flex flex-wrap justify-center sm:justify-start gap-4 my-3">
           <a
             href={pdf}
-            className="px-6 py-3 w-full rounded border border-gray-100 hover:bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 mr-4 text-sm sm:text-lg"
+            className="px-6 py-3 rounded border border-gray-100 hover:bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-sm sm:text-lg"
             target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Download CV"
           >
             Download CV
           </a>
@@ -44,7 +55,8 @@ function FrontPage({ textGradientColor }) {
                 behavior: "smooth",
               });
             }}
-            className="cursor-pointer mr-4 px-6 py-3 w-full rounded border border-gray-100 hover:bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-sm sm:text-lg"
+            className="cursor-pointer px-6 py-3 rounded border border-gray-100 hover:bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-sm sm:text-lg"
+            aria-label="Contact Me"
           >
             Contact Me
           </a>
